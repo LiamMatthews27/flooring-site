6014711734aa0488b89f50db6715d22ed3b2dd09
Liam Matthews <221818340@mycput.ac.za>
2026-05-15 15:26:24 +0200

    update

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -21,16 +21,13 @@ const testimonials = [
 const sliderSettings = {
   dots: true,
   infinite: true,
-  speed: 700,
-  slidesToShow: 3,
+  speed: 600,
+  slidesToShow: 1,
   slidesToScroll: 1,
-  centerMode: false,
   autoplay: true,
-  autoplaySpeed: 4000,
-  responsive: [
-    { breakpoint: 1024, settings: { slidesToShow: 2, centerMode: false } },
-    { breakpoint: 768, settings: { slidesToShow: 1, centerMode: false } },
-  ],
+  autoplaySpeed: 5000,
+  fade: true,
+  arrows: false,
 };
 
 const Home = () => {
