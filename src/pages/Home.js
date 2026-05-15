237d3468266ba6f9e8a909066a18c5021524911c
Liam Matthews <221818340@mycput.ac.za>
2026-05-15 15:13:26 +0200

    update

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -24,12 +24,11 @@ const sliderSettings = {
   speed: 700,
   slidesToShow: 3,
   slidesToScroll: 1,
-  centerMode: true,
-  centerPadding: "60px",
+  centerMode: false,
   autoplay: true,
   autoplaySpeed: 4000,
   responsive: [
-    { breakpoint: 1024, settings: { slidesToShow: 2, centerMode: true, centerPadding: "40px" } },
+    { breakpoint: 1024, settings: { slidesToShow: 2, centerMode: false } },
     { breakpoint: 768, settings: { slidesToShow: 1, centerMode: false } },
   ],
 };
@@ -162,7 +161,7 @@ const Home = () => {
                 className="testimonial-card-link"
               >
                 <div className="testimonial-card">
-                  <span className="quote">“</span>
+                  <span className="quote">"</span>
                   <p className="testimonial-text">{t.text}</p>
                   <b className="testimonial-author">- {t.name}</b>
                 </div>
